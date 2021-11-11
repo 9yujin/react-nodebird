9b69111f987ed90b257e3e497b31d15a5704338e
한규진 <david0218@naver.com>
2021-11-11 20:54:22 +0900

    리덕스 사가 배우기3

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,15 +1,15 @@
-import { HYDRATE } from "next-redux-wrapper";
-import { combineReducers } from "redux";
+import { HYDRATE } from 'next-redux-wrapper';
+import { combineReducers } from 'redux';
 
-import user from "./user";
-import post from "./post";
+import user from './user';
+import post from './post';
 
-//(이전상태 , 액션) => 다음상태
-const rootReducer = combineReducers({ //인덱스, 유저, 포스트 세개 합친겨
+// (이전상태, 액션) => 다음상태
+const rootReducer = combineReducers({
   index: (state = {}, action) => {
-    switch (action.type) {          //HYDRATE를 위해서 인덱스 리듀서 하나 추가한거임.(서버사이드렌더링)
+    switch (action.type) {
       case HYDRATE:
-        console.log("HYDRATE", action);
+        console.log('HYDRATE', action);
         return { ...state, ...action.payload };
       default:
         return state;
--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,35 +1,25 @@
-import { applyMiddleware, createStore, compose } from "redux";
-import { createWrapper } from "next-redux-wrapper";
-import { composeWithDevTools } from "redux-devtools-extension";
-import createSagaMiddleware from "@redux-saga/core";
-import reducer from "../reducers";
-import rootSaga from "../sagas/";
+import { applyMiddleware, createStore, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createWrapper } from 'next-redux-wrapper';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const loggerMiddleware =
-  ({ dispatch, getState }) =>
-  (next) =>
-  (action) => {
-    console.log(action);
-    return next(action);
-  };
+import reducer from '../reducers';
+import rootSaga from '../sagas';
 
 const configureStore = (context) => {
-  const sagaMiddleware = createSagaMiddleware(); //사가
-  const middlewares = [sagaMiddleware, loggerMiddleware];
-  const enhancer =
-    process.env.NODE_ENV === "production" //개발용인지 아닌지
-      ? compose(applyMiddleware(...middlewares)) //배포용일때
-      : composeWithDevTools(
-          //개발용일때,, devtool을 넣음. 이런건 nextjs 쓸때만 하는건가??
-          applyMiddleware(...middlewares)
-        );
-  const store = createStore(reducer, enhancer); //스토어ㄹ 가와
-
+  console.log(context);
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(
+      applyMiddleware(...middlewares),
+    );
+  const store = createStore(reducer, enhancer);
   store.sagaTask = sagaMiddleware.run(rootSaga);
-  사가;
   return store;
 };
 
-const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === "development" });
+const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === 'development' });
 
 export default wrapper;
